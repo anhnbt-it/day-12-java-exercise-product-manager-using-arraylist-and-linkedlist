a04d2c89d1de32f7f23a79495a173ab195ac16d8
anhnbt-it <anhnbt.it@gmail.com>
2020-10-20 16:02:23 +0700

    feat: add ProductManager class

---
+++ src/ProductManager.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/20/2020
+ * Time: 4:00 PM
+ */
+public class ProductManager {
+    List<Product> products;
+
+    public ProductManager() {
+        products = new ArrayList<>();
+    }
+
+    public void create() {
+
+    }
+
+    public void read() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+}
